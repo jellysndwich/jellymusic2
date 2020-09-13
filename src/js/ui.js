2aebcf0e4d7e52126856b3a7a4525f72db98e60d
Timur Kalandarov <jellysandwich@pm.me>
2020-09-13 14:27:30 +0600

    UI 2.0 Update

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,17 +1,46 @@
-var navDrawer = document.querySelector('.drawer');
-
-// Event Handlers
+// ONSTART EVENT
 window.onload = function () {
 	$('.window-title').addClass('effect-shine');
-}
+};
 
-// Play/Pause switch
-$('.btn-play').click(function () {
-	TweenMax.to($('.btn-play'), 0.2, { x: 20, opacity: 0, scale: 0.3, display: 'none', ease: Power2.easeInOut });
-	TweenMax.fromTo($('.btn-pause'), 0.2, { x: -20, opacity: 0, scale: 0.3, display: 'none' }, { x: 0, opacity: 1, scale: 1, display: 'block', ease: Power2.easeInOut });
-});
+(function ($) {
+	"use strict";
 
-$('.btn-pause').click(function () {
-	TweenMax.to($('.btn-pause'), 0.2, { x: 20, opacity: 0, display: 'none', scale: 0.3, ease: Power2.easeInOut });
-	TweenMax.fromTo($('.btn-play'), 0.2, { x: -20, opacity: 0, scale: 0.3, display: 'none' }, { x: 0, opacity: 1, display: 'block', scale: 1, ease: Power2.easeInOut });
-});
+	//Page cursors
+
+	document.getElementsByTagName("body")[0].addEventListener("mousemove", function (n) {
+		t.style.left = n.clientX + "px",
+			t.style.top = n.clientY + "px",
+			e.style.left = n.clientX + "px",
+			e.style.top = n.clientY + "px",
+			i.style.left = n.clientX + "px",
+			i.style.top = n.clientY + "px"
+	});
+	var t = document.getElementById("cursor"),
+		e = document.getElementById("cursor2"),
+		i = document.getElementById("cursor3");
+	function n(t) {
+		e.classList.add("hover"), i.classList.add("hover")
+	}
+	function s(t) {
+		e.classList.remove("hover"), i.classList.remove("hover")
+	}
+	s();
+	for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+		o(r[a])
+	}
+	function o(t) {
+		t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+	}
+
+
+	// Playlist page
+
+	$(".playlist-text").on('click', function () {
+		$("body").addClass("playlist-on");
+	});
+	$(".playlist-close").on('click', function () {
+		$("body").removeClass("playlist-on");
+	});
+
+})(jQuery);
